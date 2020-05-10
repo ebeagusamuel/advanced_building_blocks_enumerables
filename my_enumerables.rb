618e4ec08f0f24988de11460f15ed11f45ac2082
ebeagusamuel <samuelebeagu@gmail.com>
2020-05-10 02:29:45 +0100

    Add my_each_with_index method to the enumerables module

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,26 +1,32 @@
-module Enumerables
-    def my_each(arr)
-        if arr.is_a?(Array)
-            0.upto arr.length - 1 do |value|
-                if block_given?
-                    yield(arr[value]) 
-                else
-                    puts arr[value]
-                end
-            end
-        end
-            
-        if arr.is_a?(Range)
-            for val in arr
-                if block_given?
-                    yield(val)
-                else
-                    puts val
-                end
-            end
-        end 
-    end    
 
+	def my_each(arr)
+		if arr.is_a?(Array)
+			0.upto arr.length - 1 do |val|
+				if block_given?
+					yield(arr[val]) 
+				else
+					arr[val]
+				end
+			end
+		end
+				
+		if arr.is_a?(Range)
+			for val in arr
+				if block_given?
+					yield(val)
+				else
+					val
+				end
+			end
+		end 
+	end    
 
-end
-
+	def my_each_with_index(arr)
+		0.upto arr.length - 1 do |val|
+			if block_given?
+				yield(arr[val], val)
+			else
+				puts "#{arr[val]} #{val}"
+			end
+		end
+	end
