1dd40b6aa9f916aab06447dc73eedccc694de5ad
ebeagusamuel <samuelebeagu@gmail.com>
2020-05-11 11:05:34 +0100

    Add the my_select method to the Enumerable module

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,32 +1,53 @@
-
-	def my_each(arr)
-		if arr.is_a?(Array)
-			0.upto arr.length - 1 do |val|
+module Enumerable
+	def my_each
+		if is_a?(Array)
+			0.upto size - 1 do |val|
 				if block_given?
-					yield(arr[val]) 
-				else
-					arr[val]
+					yield(self[val]) 
 				end
 			end
-		end
-				
-		if arr.is_a?(Range)
-			for val in arr
+		elsif is_a?(Range)
+			for val in self
 				if block_given?
 					yield(val)
-				else
-					val
 				end
+			end
+		else
+			hash_keys = self.keys
+			0.upto hash_keys.length - 1 do |num|
+				key = hash_keys[num]
+				yield(key, self[key])
 			end
 		end 
 	end    
 
-	def my_each_with_index(arr)
-		0.upto arr.length - 1 do |val|
-			if block_given?
-				yield(arr[val], val)
-			else
-				puts "#{arr[val]} #{val}"
-			end
+	def my_each_with_index
+		0.upto size - 1 do |val|
+			yield(self[val], val)
 		end
 	end
+
+	def my_select
+		return_arr = []
+		return_hash = {}
+		if is_a?(Hash)
+			self.my_each do |key, val|
+				if yield(key, val)
+					return_hash[key] = val
+				end
+			end
+			return_hash
+		else
+			self.my_each do |val|
+				if yield(val)
+					return_arr.push(val)
+				end
+			end
+			return_arr
+		end
+	end
+
+end
+x = 1..6
+x.my_select{ |v| v >= 3}
+	
